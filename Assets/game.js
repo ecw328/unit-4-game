2ed4fdfbf5b6d4e8597cfc8761aa49237a54095d
unknown <eric.wilson1015@icloud.com>
2019-04-03 10:16:46 -0500

    updated game section and started JS

---
+++ Assets/game.js
@@ -0,0 +1,7 @@
+function Game(){
+    var game = document.querySelector("section#game");
+    var score = game.querySelector("section#game span.score");
+    var high_scores = game.querySelector("section#game ol.high-scores");
+    var time = game.querySelector("section#game span.time");
+    var start = game.querySelector("section#game span.start");
+)
